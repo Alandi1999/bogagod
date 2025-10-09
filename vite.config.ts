a53c03e8554c3723b93ea854f4d73262c86ca929
strix9999 <strixclothes@gmail.com>
2025-10-09 15:44:10 -0300

    Testing

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/bogagod/' : '/',
+  base: command === 'build' ? '/bogagod/' : '/',
   server: {
     port: 3000,
     open: true
   },
   publicDir: 'public',
   assetsInclude: ['**/*.webp', '**/*.png']
-})
+}))
 
